1da5026f2c3a741c0e1d2e81ae495e63c2b969d6
Stanislav.Presnetsov <Stanislav.Presnetsov@ocrv.ru>
2021-08-09 13:06:22 +0800

    fix: скорректирован вывод длинного наименования ОЕ в штампе при выгрузке в файл

---
+++ src/canvas/index.js
@@ -0,0 +1,327 @@
+import { BORDER_WIDTH, IND_HEIGHT } from "../constants";
+
+/**
+ * Перевод HTML на канву
+ * @param {HTMLElement} root
+ * @param {string} ofmTitle
+ * @param {number} width
+ * @param {number} height
+ * @param {Map} blocksMap
+ * @param {Map} blockParamsMap
+ * @param {Map} linesMap
+ * @param {HTMLElement} stampBlock
+ */
+export const translateHTMLToCanvas = ( root,
+                                       ofmTitle,
+                                       width,
+                                       height,
+                                       blocksMap,
+                                       blockParamsMap,
+                                       linesMap,
+                                       stampBlock ) => {
+
+    const canvas = document.createElement('canvas');
+    canvas.width = width;
+    canvas.height = height;
+    canvas.style.display = 'none';
+
+    root.appendChild(canvas);
+
+    const ctx = canvas.getContext('2d');
+    if (blockParamsMap) {
+        blockParamsMap.forEach((blockParams) => {
+            const {x, y, width, height, innerPaddingLeft, innerPaddingRight, borderWidth, borderStyle, backgroundColor, title, functions, indicators} = blockParams;
+            canvasDrawRect(ctx, x, y, width, height, innerPaddingLeft, innerPaddingRight, borderWidth, borderStyle, backgroundColor, title, functions, indicators);
+        });
+    }
+
+    if (stampBlock) {
+        canvasDrawStamp(ctx, stampBlock.style, stampBlock.childNodes[0], stampBlock.childNodes);
+    }
+
+    if (linesMap) {
+        linesMap.forEach((line) => {
+            line.parts.forEach((part, i) => {
+                if (i !== line.parts.length - 1) {
+                    canvasDrawLine(ctx, part, line.parts[i+1], line.lineStyle, line.lineColor);
+                }
+            });
+        });
+    }
+
+    return canvas
+};
+
+/**
+ * Вывод блока на канву
+ * @param {Object} ctx
+ * @param {number} x
+ * @param {number} y
+ * @param {number} width
+ * @param {number} height
+ * @param {number} innerPaddingLeft
+ * @param {number} innerPaddingRight
+ * @param {number} borderWidth
+ * @param {number} borderStyle
+ * @param {string} backgroundColor
+ * @param {Object} title
+ * @param {Object[]} functions
+ * @param {Object[]} indicators
+ */
+const canvasDrawRect = (ctx, x, y, width, height, innerPaddingLeft, innerPaddingRight, borderWidth, borderStyle, backgroundColor, title, functions, indicators) => {
+    ctx.beginPath();
+    let spaceBetweenLines = 0;
+    let indVShift = 0;
+    width += innerPaddingLeft + innerPaddingRight + 1;
+    height += 2;
+    const rectShift = 0.5;
+
+    switch (borderStyle) {
+        case 'solid':
+            ctx.lineWidth = borderWidth;
+            break;
+        case 'double':
+            ctx.lineWidth = '1';
+            const parsedBorderWidth = borderWidth;
+            if (parsedBorderWidth === BORDER_WIDTH.first) {
+                spaceBetweenLines = BORDER_WIDTH.first;
+            } else if (parsedBorderWidth === BORDER_WIDTH.second) {
+                spaceBetweenLines = BORDER_WIDTH.second;
+            }
+            indVShift += 1;
+            break;
+        default:
+            ctx.lineWidth = '1';
+    }
+    ctx.fillStyle = backgroundColor;
+    ctx.fillRect(x, y, width + spaceBetweenLines, height + spaceBetweenLines);
+    ctx.fillStyle = 'black';
+    ctx.strokeRect(x + rectShift, y + rectShift, width + spaceBetweenLines, height + spaceBetweenLines);
+    if (spaceBetweenLines) {
+        ctx.strokeRect(x + spaceBetweenLines + rectShift, y + spaceBetweenLines + rectShift, width - spaceBetweenLines, height - spaceBetweenLines);
+    }
+
+    // заголовок
+    const textX = x + innerPaddingLeft + borderWidth;
+    let textY = y + borderWidth + title.paddingTop;
+    const textWidth = width - innerPaddingLeft - innerPaddingRight;
+    const titleTextObject = {...title, height: !functions.length ? height - IND_HEIGHT - title.height: title.height }
+    textY = rectDrawText( ctx,
+        textX,
+        textY,
+        textWidth,
+        innerPaddingLeft,
+        innerPaddingRight,
+        0,
+        titleTextObject);
+
+    // Функции
+    if (functions.length > 1) {
+        functions.forEach((func) => {
+            textY = rectDrawText( ctx,
+                textX,
+                textY,
+                textWidth,
+                innerPaddingLeft,
+                innerPaddingRight,
+                4,
+                func);
+        });
+    }
+
+    // индикаторы
+    if (indicators. length > 1) {
+        let indicatorX = x + 1;
+        const indicatorY = y + height + borderWidth + indVShift;
+        ctx.lineWidth = '1';
+        let prevIndX = 0;
+        indicators.reverse().forEach((ind) => {
+            indicatorX += ind.x - prevIndX;
+            prevIndX = ind.x;
+            ctx.fillStyle = 'lightyellow';
+            ctx.strokeRect(indicatorX, indicatorY, ind.width, ind.height);
+            ctx.fillRect(indicatorX, indicatorY, ind.width, ind.height);
+
+            ctx.fillStyle = 'blue';
+            ctx.textAlign = 'center';
+            ctx.font = ind.font;
+            ctx.fillText(ind.text, indicatorX + ind.width / 2, indicatorY + ind.height / 2);
+        });
+    }
+};
+
+/**
+ *
+ * @param ctx
+ * @param left
+ * @param top
+ * @param width
+ * @param paddingLeft
+ * @param paddingRight
+ * @param lineSpacing
+ * @param textObject
+ * @return {*}
+ */
+const rectDrawText = (ctx, left, top, width, paddingLeft, paddingRight, lineSpacing, textObject) => {
+    const words = textObject.text.split(' ');
+    const textX = left + Math.trunc(width / 2);
+    let line = '';
+    let textY = top + Math.trunc(textObject.paddingTop * 2 + textObject.height / 2);
+
+    ctx.fillStyle = 'black';
+    ctx.textAlign = textObject.textAlign;
+    ctx.textBaseline = 'middle';
+    ctx.font = textObject.font;
+    words.forEach((word) => {
+        const tmpLine = line + word + ' ';
+        const tmpWidth = ctx.measureText(tmpLine).width;
+        if (tmpWidth < width) {
+            line = tmpLine;
+        } else {
+            ctx.fillText(line, textX, textY);
+            line = word + ' ';
+            textY += textObject.height + lineSpacing;
+        }
+    });
+    if (line) {
+        ctx.fillText(line, textX, textY);
+        textY += textObject.height + textObject.paddingBottom;
+    }
+    return textY;
+};
+
+/**
+ * Отрисовка линии
+ * @param ctx
+ * @param pointFrom
+ * @param pointTo
+ * @param lineStyle
+ * @param lineColor
+ */
+const canvasDrawLine = (ctx, pointFrom, pointTo, lineStyle, lineColor) => {
+    ctx.beginPath();
+    ctx.moveTo(pointFrom.x + 0.5, pointFrom.y + 0.5);
+    ctx.lineTo(pointTo.x + 0.5, pointTo.y + 0.5);
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = 1;
+    if (lineStyle !== 'solid') {
+        ctx.setLineDash([5, 5]);
+    }
+    ctx.stroke();
+};
+
+/**
+ *
+ * @param ctx
+ * @param left
+ * @param top
+ * @param width
+ * @param textObject
+ * @return {*}
+ */
+const stampDrawTitle = (ctx, left, top, width, textObject) => {
+    const words = textObject.text.split(' ');
+    let line = '';
+    let textY = top
+
+    ctx.fillStyle = 'black';
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+    ctx.font = textObject.font;
+    words.forEach((word) => {
+        const tmpLine = line + word + ' ';
+        const tmpWidth = ctx.measureText(tmpLine).width;
+        if (tmpWidth < width) {
+            line = tmpLine;
+        } else {
+            ctx.fillText(line, left, textY);
+            line = word + ' ';
+            textY += textObject.height + 1;
+        }
+    });
+    if (line) {
+        ctx.fillText(line, left, textY);
+        textY += textObject.height + textObject.paddingBottom;
+    }
+
+    return textY;
+}
+
+/**
+ *
+ * @param ctx
+ * @param left
+ * @param top
+ * @param propNameObject
+ * @param propValueObject
+ * @return {*}
+ */
+const stampDrawProp = (ctx, left, top, propNameObject, propValueObject) => {
+    let textLeft = left;
+    ctx.textAlign = 'left';
+    ctx.textBaseline = 'middle';
+    ctx.font = propNameObject.font;
+    ctx.fillText(propNameObject.text, textLeft, top);
+
+    textLeft += propNameObject.width;
+    ctx.textAlign = 'left';
+    ctx.font = propValueObject.font;
+    ctx.fillText(propValueObject.text, textLeft, top);
+
+    return top + propNameObject.height + propNameObject.paddingBottom
+};
+
+/**
+ *
+ * @param ctx
+ * @param stampStyle
+ * @param stampTitle
+ * @param stampRows
+ */
+const canvasDrawStamp = (ctx, stampStyle, stampTitle, stampRows) => {
+    ctx.beginPath();
+    const textX = parseInt(stampStyle.left);
+    let textY = parseInt(stampStyle.top);
+    const width = parseInt(stampStyle.width);
+    const titleStyle = window.getComputedStyle(stampTitle);
+    const titleObject = {
+        font: `${titleStyle.fontSize} ${titleStyle.fontFamily}`,
+        text: stampTitle.textContent,
+        height: 20,
+        paddingTop: parseInt(titleStyle.paddingTop, 10),
+        paddingBottom: parseInt(titleStyle.paddingBottom, 10),
+        textAlign: 'left'
+    };
+    textY = stampDrawTitle( ctx,
+                            textX,
+                            textY,
+                            width,
+                            titleObject );
+    stampRows.forEach((stampRow, index) => {
+        if (index > 0) {
+            const propNameStyle = window.getComputedStyle(stampRow.childNodes[0])
+            const propNameObject = {
+                font: `bold ${propNameStyle.fontSize} ${propNameStyle.fontFamily}`,
+                text: stampRow.childNodes[0].textContent,
+                width: parseInt(propNameStyle.width, 10),
+                height: parseInt(propNameStyle.height, 10),
+                paddingTop: parseInt(propNameStyle.paddingTop, 10),
+                paddingBottom: parseInt(propNameStyle.paddingBottom, 10),
+            };
+            const propValueStyle = window.getComputedStyle(stampRow.childNodes[1])
+            const propValueObject = {
+                font: `${propValueStyle.fontSize} ${propValueStyle.fontFamily}`,
+                text: stampRow.childNodes[1].textContent,
+                width: parseInt(propValueStyle.width, 10),
+                height: parseInt(propValueStyle.height, 10),
+                paddingTop: parseInt(propValueStyle.paddingTop, 10),
+                paddingBottom: parseInt(propValueStyle.paddingBottom, 10),
+            };
+            textY = stampDrawProp( ctx,
+                textX,
+                textY,
+                propNameObject,
+                propValueObject);
+        }
+    });
+};
--- src/index.js
+++ src/index.js
@@ -36,6 +36,7 @@ import {
 } from "./stamp";
 import FileSaver from 'file-saver';
 import 'canvas-toBlob';
+import { translateHTMLToCanvas } from "./canvas";
 
 // Инициализация отрисовки схемы
 const maxInlineCount = 4;
@@ -94,10 +95,12 @@ export const drawScheme = () => {
   });
 
   // отрисовка приписного штата
+  let verticalShift = getVerticalShiftFromChildren(parent.children, blockParamsMap) + IND_HEIGHT + V_SPACE_BETWEEN_BLOCKS;
   //TODO добавить вывод приписного штата
+  drawAssignedStaffBlocks(blocksMap, blockParamsMap, parent, parentBlockParams, governanceAreaMap, verticalShift);
 
   // отрисовка структурных подразделений
-  const verticalShift = getVerticalShiftFromChildren(parent.children, blockParamsMap) + IND_HEIGHT + V_SPACE_BETWEEN_BLOCKS;
+  verticalShift = getVerticalShiftFromChildren(parent.children, blockParamsMap) + IND_HEIGHT + V_SPACE_BETWEEN_BLOCKS;
   drawStructuralUnitBlocks(blocksMap, blockParamsMap, parent, parentBlockParams, governanceAreaMap, verticalShift);
 
   const fullWidth = getHorizontalShiftFromChildren(parent.children, blockParamsMap);
@@ -124,7 +127,10 @@ export const drawScheme = () => {
 
   // формирование канвы для получения изображения
   if (toImage) {
-    translateHTMLToCanvasToImage(document.body, ofmTitle, fullWidth, fullHeight, blocksMap, blockParamsMap, linesMap, stampBlock);
+    const canvas = translateHTMLToCanvas(document.body, ofmTitle, fullWidth, fullHeight, blocksMap, blockParamsMap, linesMap, stampBlock);
+    canvas.toBlob((blob) => {
+      FileSaver.saveAs(blob, `${ofmTitle}.png`);
+    });
   }
 
   if (toPdf) {
@@ -331,6 +337,17 @@ const drawDeputy = (blocksMap, blockParamsMap, parent, parentParams) => {
 
   return verticalDiff > 0 ? verticalDiff : 0;
 };
+
+
+const drawAssignedStaffBlocks = ( blocksMap,
+                                  blockParamsMap,
+                                  parent,
+                                  parentBlockParams,
+                                  governanceBlocksArea,
+                                  initialVerticalShift ) => {
+
+
+}
 
 /**
  *
@@ -683,317 +700,5 @@ const drawAreaSeparator = (areaMap, fullWidth) => {
 const saveBlockParamsMapToDOM = () => {
 
 };
-
-/**
- * Перевод HTML на канву
- * @param {HTMLElement} root
- * @param {string} ofmTitle
- * @param {number} width
- * @param {number} height
- * @param {Map} blocksMap
- * @param {Map} blockParamsMap
- * @param {Map} linesMap
- * @param {HTMLElement} stampBlock
- */
-const translateHTMLToCanvasToImage = ( root,
-                                       ofmTitle,
-                                       width,
-                                       height,
-                                       blocksMap,
-                                       blockParamsMap,
-                                       linesMap,
-                                       stampBlock ) => {
-
-  const canvas = document.createElement('canvas');
-  canvas.width = width;
-  canvas.height = height;
-  canvas.style.display = 'none';
-
-  root.appendChild(canvas);
-
-  const ctx = canvas.getContext('2d');
-  if (blockParamsMap) {
-    blockParamsMap.forEach((blockParams) => {
-      const {x, y, width, height, innerPaddingLeft, innerPaddingRight, borderWidth, borderStyle, backgroundColor, title, functions, indicators} = blockParams;
-      canvasDrawRect(ctx, x, y, width, height, innerPaddingLeft, innerPaddingRight, borderWidth, borderStyle, backgroundColor, title, functions, indicators);
-    });
-  }
-
-  if (stampBlock) {
-    canvasDrawStamp(ctx, stampBlock.style, stampBlock.childNodes[0], stampBlock.childNodes);
-  }
-
-  if (linesMap) {
-    linesMap.forEach((line) => {
-      line.parts.forEach((part, i) => {
-        if (i !== line.parts.length - 1) {
-          canvasDrawLine(ctx, part, line.parts[i+1], line.lineStyle, line.lineColor);
-        }
-      });
-    });
-  }
-
-  canvas.toBlob((blob) => {
-    FileSaver.saveAs(blob, `${ofmTitle}.png`);
-  });
-};
-
-
-
-/**
- * Вывод блока на канву
- * @param {Object} ctx
- * @param {number} x
- * @param {number} y
- * @param {number} width
- * @param {number} height
- * @param {number} innerPaddingLeft
- * @param {number} innerPaddingRight
- * @param {number} borderWidth
- * @param {number} borderStyle
- * @param {string} backgroundColor
- * @param {Object} title
- * @param {Object[]} functions
- * @param {Object[]} indicators
- */
-const canvasDrawRect = (ctx, x, y, width, height, innerPaddingLeft, innerPaddingRight, borderWidth, borderStyle, backgroundColor, title, functions, indicators) => {
-  ctx.beginPath();
-  let spaceBetweenLines = 0;
-  let indVShift = 0;
-  width += innerPaddingLeft + innerPaddingRight + 1;
-  height += 2;
-  const rectShift = 0.5;
-
-  switch (borderStyle) {
-    case 'solid':
-      ctx.lineWidth = borderWidth;
-      break;
-    case 'double':
-      ctx.lineWidth = '1';
-      const parsedBorderWidth = borderWidth;
-      if (parsedBorderWidth === BORDER_WIDTH.first) {
-        spaceBetweenLines = BORDER_WIDTH.first;
-      } else if (parsedBorderWidth === BORDER_WIDTH.second) {
-        spaceBetweenLines = BORDER_WIDTH.second;
-      }
-      indVShift += 1;
-      break;
-    default:
-      ctx.lineWidth = '1';
-  }
-  ctx.fillStyle = backgroundColor;
-  ctx.fillRect(x, y, width + spaceBetweenLines, height + spaceBetweenLines);
-  ctx.fillStyle = 'black';
-  ctx.strokeRect(x + rectShift, y + rectShift, width + spaceBetweenLines, height + spaceBetweenLines);
-  if (spaceBetweenLines) {
-    ctx.strokeRect(x + spaceBetweenLines + rectShift, y + spaceBetweenLines + rectShift, width - spaceBetweenLines, height - spaceBetweenLines);
-  }
-
-  // заголовок
-  const textX = x + innerPaddingLeft + borderWidth;
-  let textY = y + borderWidth + title.paddingTop;
-  const textWidth = width - innerPaddingLeft - innerPaddingRight;
-  const titleTextObject = {...title, height: !functions.length ? height - IND_HEIGHT - title.height: title.height }
-  textY = rectDrawText( ctx,
-                        textX,
-                        textY,
-                        textWidth,
-                        innerPaddingLeft,
-                        innerPaddingRight,
-                        0,
-                        titleTextObject);
-
-  // Функции
-  if (functions.length > 1) {
-    functions.forEach((func) => {
-      textY = rectDrawText( ctx,
-                            textX,
-                            textY,
-                            textWidth,
-                            innerPaddingLeft,
-                            innerPaddingRight,
-                            4,
-                            func);
-    });
-  }
-
-  // индикаторы
-  if (indicators. length > 1) {
-    let indicatorX = x + 1;
-    const indicatorY = y + height + borderWidth + indVShift;
-    ctx.lineWidth = '1';
-    let prevIndX = 0;
-    indicators.reverse().forEach((ind) => {
-      indicatorX += ind.x - prevIndX;
-      prevIndX = ind.x;
-      ctx.fillStyle = 'lightyellow';
-      ctx.strokeRect(indicatorX, indicatorY, ind.width, ind.height);
-      ctx.fillRect(indicatorX, indicatorY, ind.width, ind.height);
-
-      ctx.fillStyle = 'blue';
-      ctx.textAlign = 'center';
-      ctx.font = ind.font;
-      ctx.fillText(ind.text, indicatorX + ind.width / 2, indicatorY + ind.height / 2);
-    });
-  }
-};
-
-/**
- *
- * @param ctx
- * @param left
- * @param top
- * @param width
- * @param paddingLeft
- * @param paddingRight
- * @param lineSpacing
- * @param textObject
- * @return {*}
- */
-const rectDrawText = (ctx, left, top, width, paddingLeft, paddingRight, lineSpacing, textObject) => {
-  const words = textObject.text.split(' ');
-  const maxWidth = width;
-  const textX = left + Math.trunc(maxWidth / 2);
-  let line = '';
-  let textY = top + Math.trunc(textObject.paddingTop * 2 + textObject.height / 2);
-
-  ctx.fillStyle = 'black';
-  ctx.textAlign = textObject.textAlign;
-  ctx.textBaseline = 'middle';
-  ctx.font = textObject.font;
-  words.forEach((word) => {
-    const tmpLine = line + word + ' ';
-    const tmpWidth = ctx.measureText(tmpLine).width;
-    if (tmpWidth < maxWidth) {
-      line = tmpLine;
-    } else {
-      ctx.fillText(line, textX, textY);
-      line = word + ' ';
-      textY += textObject.height + lineSpacing;
-    }
-  });
-  if (line) {
-    ctx.fillText(line, textX, textY);
-    textY += textObject.height + textObject.paddingBottom;
-  }
-  return textY;
-};
-
-/**
- * Отрисовка линии
- * @param ctx
- * @param pointFrom
- * @param pointTo
- * @param lineStyle
- * @param lineColor
- */
-const canvasDrawLine = (ctx, pointFrom, pointTo, lineStyle, lineColor) => {
-  ctx.beginPath();
-  ctx.moveTo(pointFrom.x + 0.5, pointFrom.y + 0.5);
-  ctx.lineTo(pointTo.x + 0.5, pointTo.y + 0.5);
-  ctx.strokeStyle = lineColor;
-  ctx.lineWidth = 1;
-  if (lineStyle !== 'solid') {
-    ctx.setLineDash([5, 5]);
-  }
-  ctx.stroke();
-};
-
-/**
- *
- * @param ctx
- * @param left
- * @param top
- * @param textObject
- * @return {*}
- */
-const stampDrawTitle = (ctx, left, top, textObject) => {
-  ctx.fillStyle = 'black';
-  ctx.textAlign = 'left';
-  ctx.textBaseline = 'middle';
-  ctx.font = textObject.font;
-  ctx.fillText(textObject.text, left, top);
-
-  return top + textObject.height + textObject.paddingBottom;
-}
-
-/**
- *
- * @param ctx
- * @param left
- * @param top
- * @param propNameObject
- * @param propValueObject
- * @return {*}
- */
-const stampDrawProp = (ctx, left, top, propNameObject, propValueObject) => {
-  let textLeft = left;
-  ctx.textAlign = 'left';
-  ctx.textBaseline = 'middle';
-  ctx.font = propNameObject.font;
-  ctx.fillText(propNameObject.text, textLeft, top);
-
-  textLeft += propNameObject.width;
-  ctx.textAlign = 'left';
-  ctx.font = propValueObject.font;
-  ctx.fillText(propValueObject.text, textLeft, top);
-
-  return top + propNameObject.height + propNameObject.paddingBottom
-};
-
-/**
- *
- * @param ctx
- * @param stampStyle
- * @param stampTitle
- * @param stampRows
- */
-const canvasDrawStamp = (ctx, stampStyle, stampTitle, stampRows) => {
-  ctx.beginPath();
-  const textX = parseInt(stampStyle.left);
-  let textY = parseInt(stampStyle.top);
-  const titleStyle = window.getComputedStyle(stampTitle);
-  const titleObject = {
-    font: `${titleStyle.fontSize} ${titleStyle.fontFamily}`,
-    text: stampTitle.textContent,
-    height: parseInt(titleStyle.height, 10),
-    paddingTop: parseInt(titleStyle.paddingTop, 10),
-    paddingBottom: parseInt(titleStyle.paddingBottom, 10),
-    textAlign: 'left'
-  };
-  textY = stampDrawTitle( ctx,
-                          textX,
-                          textY,
-                          titleObject );
-  stampRows.forEach((stampRow, index) => {
-    if (index > 0) {
-      const propNameStyle = window.getComputedStyle(stampRow.childNodes[0])
-      const propNameObject = {
-        font: `bold ${propNameStyle.fontSize} ${propNameStyle.fontFamily}`,
-        text: stampRow.childNodes[0].textContent,
-        width: parseInt(propNameStyle.width, 10),
-        height: parseInt(propNameStyle.height, 10),
-        paddingTop: parseInt(propNameStyle.paddingTop, 10),
-        paddingBottom: parseInt(propNameStyle.paddingBottom, 10),
-      };
-      const propValueStyle = window.getComputedStyle(stampRow.childNodes[1])
-      const propValueObject = {
-        font: `${propValueStyle.fontSize} ${propValueStyle.fontFamily}`,
-        text: stampRow.childNodes[1].textContent,
-        width: parseInt(propValueStyle.width, 10),
-        height: parseInt(propValueStyle.height, 10),
-        paddingTop: parseInt(propValueStyle.paddingTop, 10),
-        paddingBottom: parseInt(propValueStyle.paddingBottom, 10),
-      };
-      textY = stampDrawProp( ctx,
-          textX,
-          textY,
-          propNameObject,
-          propValueObject);
-    }
-  });
-};
-
-
 
 drawScheme();
